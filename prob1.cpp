bc82e0bf2251da650678f515ea5e4483676f8807
hjl971230 <55931023+hjl971230@users.noreply.github.com>
2019-12-27 18:08:46 +0900

    Add files via upload

--- prob1.cpp
+++ prob1.cpp
@@ -20,8 +20,11 @@ int main()
 	int maxlength = 0;//사이클 길이 중 최대 길이
 	int Loopnum = 0;//내부에서 반복할 때의 수
 	info iarr[4] = { 0 };
+	info oarr[4] = { 0 };
 	ifstream save;
+	ifstream ansf;
 	save.open("test.txt");
+	ansf.open("checktest.txt");
 	if (save.is_open())
 	{
 		for (int i = 0; i < 4; i++)
@@ -29,53 +32,64 @@ int main()
 			save >> iarr[i].firstnum;
 			save >> iarr[i].secondnum;
 			save >> iarr[i].ans;
-			cout << iarr[i].firstnum <<" "<< iarr[i].secondnum <<" "<< iarr[i].ans << endl;
+			//cout << iarr[i].firstnum <<" "<< iarr[i].secondnum <<" "<< iarr[i].ans << endl;
 		}
 		
 		save.close();
 	}
-	while (1)
+	if (ansf.is_open())
 	{
-		cin >> i ;//두 수를 입력받기, -1 입력 시 종료
-		if (i == -1){break;}
-		cin >> j;
-		if (j == -1){break;}
-		if (i > j)//첫번쨰 수가 두번째 수보다 큰 경우
+		for (int i = 0; i < 4; i++)
 		{
-			temp = i;//두 수를 서로 교환
-			i = j;
-			j = temp;
+			ansf >> oarr[i].firstnum;
+			ansf >> oarr[i].secondnum;
+			ansf >> oarr[i].ans;
+			//cout << iarr[i].firstnum << " " << iarr[i].secondnum << " " << iarr[i].ans << endl;
 		}
+
+		ansf.close();
+	}
+	//while (1)
+	{
+		//cin >> i ;//두 수를 입력받기, -1 입력 시 종료
+		//if (i == -1){break;}
+		//cin >> j;
+		//if (j == -1){break;}
+		//if (i > j)//첫번쨰 수가 두번째 수보다 큰 경우
+		//{
+		//	temp = i;//두 수를 서로 교환
+		//	i = j;
+		//	j = temp;
+		//}
 		startsec = time(NULL);//time(NULL)로 현재 시간 체크 가능
 		cout << "Algorirhm Caculate..." << endl;
 		Sleep(2000);
-		for (int a = i; a <= j; a++)//i부터 j까지 반복
-		{
-			Loopnum = a;//a를 직접 바꾸면 반복문을 정상 진행 할 수 없으므로 다른 공간 활용
-			if (Loopnum == 1) { length++; }//시작 시 1 인 경우 길이 1 증가
-			while (Loopnum != 1)//1이 아니면 반복
-			{
-				length++;//길이 증가
-				if (Loopnum % 2 == 1) { Loopnum = (3 * Loopnum) + 1; }//짝수 일 때
-				else if (Loopnum % 2 == 0) { Loopnum /= 2; }//홀수 일 때
-				if (Loopnum == 1) { length++; }//1이 되면 길이 1 증가
-			}
-			if (length > maxlength) { maxlength = length; }//계산 후 길이가 저장된 최대 길이보다 큰 경우 그 값으로 최대 길이 저장
-			length = 0;//가변 길이 0으로 초기화
-		}
-		
-		cout << i << " " << j << " " << maxlength << endl;// i j 최대길이 로 출력
+		//for (int a = i; a <= j; a++)//i부터 j까지 반복
+		//{
+		//	Loopnum = a;//a를 직접 바꾸면 반복문을 정상 진행 할 수 없으므로 다른 공간 활용
+		//	if (Loopnum == 1) { length++; }//시작 시 1 인 경우 길이 1 증가
+		//	while (Loopnum != 1)//1이 아니면 반복
+		//	{
+		//		length++;//길이 증가
+		//		if (Loopnum % 2 == 1) { Loopnum = (3 * Loopnum) + 1; }//짝수 일 때
+		//		else if (Loopnum % 2 == 0) { Loopnum /= 2; }//홀수 일 때
+		//		if (Loopnum == 1) { length++; }//1이 되면 길이 1 증가
+		//	}
+		//	if (length > maxlength) { maxlength = length; }//계산 후 길이가 저장된 최대 길이보다 큰 경우 그 값으로 최대 길이 저장
+		//	length = 0;//가변 길이 0으로 초기화
+		//}
+		//cout << i << " " << j << " " << maxlength << endl;// i j 최대길이 로 출력
 		
 		cout << "Algorirhm Error Check..." << endl;
 		Sleep(2000);
 
 		for (int x = 0; x < 4; x++)//미리 저장한 알고리즘 정답과 일치하는지 검사
 		{
-			if ((iarr[x].firstnum == i && iarr[x].secondnum == j) && maxlength != iarr[x].ans)
+			if ((iarr[x].firstnum == oarr[x].firstnum && iarr[x].secondnum == oarr[x].secondnum) && oarr[x].ans != iarr[x].ans)
 			{
 				cout << "Algorithm error" << endl;
 			}
-			else if ((iarr[x].firstnum == i && iarr[x].secondnum == j) && maxlength == iarr[x].ans)
+			else if ((iarr[x].firstnum == oarr[x].firstnum && iarr[x].secondnum == oarr[x].secondnum) && oarr[x].ans == iarr[x].ans)
 			{
 				cout << "Algorithm correct" << endl;
 			}
--- prob3.cpp
+++ prob3.cpp
@@ -21,29 +21,28 @@ int main()
 	int tmp_pay[1000]{ 0 };
 	int exchange=0;
 
-	cin >> student_count;
+	cin >> student_count;//사람 수
 
-	for (int i = 0; i < student_count; i++)
+	for (int i = 0; i < student_count; i++)//수대로 가격 입력받기
 	{
 		cin >> arr[i];
-		all_pay += arr[i];
+		all_pay += arr[i];//받은 값 더하기
 	}
-	sort(arr, end(arr), [] (int a,int b){return a > b; });
+	//sort(arr, end(arr), [] (int a,int b){return a > b; });
 	for (int i = 0; i < student_count; i++)
 	{
 		cout << arr[i] << " ";
 	}
-	n_pay = all_pay / student_count;
+	n_pay = all_pay / student_count;//전체를 학생 수만큼 나누기
 	for (int i = 0; i < student_count; i++)
 	{
 		tmp_pay[i] = n_pay;
-		
 	}
 	
-	all_pay %= student_count;
+	all_pay %= student_count;//사람 수만큼 나눈 나머지
 	for (int i = 0; i < all_pay; i++)
 	{
-		tmp_pay[i]++;
+		tmp_pay[i]++;//이동 금액 증가
 	}
 
 	for (int i = 0; i < student_count; i++)
